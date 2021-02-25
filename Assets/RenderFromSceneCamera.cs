890fee36a710f546c47314570720786683fa4a33
Gustorvo <31797378+Gustorvo@users.noreply.github.com>
2021-02-25 13:49:51 +0100

    backup before moving to unity 2020.2

---
+++ Assets/RenderFromSceneCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RenderFromSceneCamera : MonoBehaviour
+{
+    // get scene camera
+    public Camera cam = null;
+    public RenderTexture myRenderTexture;
+    //Camera.current 
+
+    private void RenderSceneCamera()
+    {
+        if (cam == null)
+            cam = SceneView.lastActiveSceneView.camera;        
+        cam.targetTexture = myRenderTexture;
+       // cam.giz
+    }
+    private void Start()
+    {
+        RenderSceneCamera();
+    }
+
+    private void LateUpdate()
+    {
+        if (cam != null)
+            cam.Render();
+    }
+    
+}
